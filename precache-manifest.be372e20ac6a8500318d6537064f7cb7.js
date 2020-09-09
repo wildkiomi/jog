87551fa91f8030f857d79c1c3c63bdb930c1de4d
wildkiomi <whisann.lie14@gmail.com>
2020-09-09 13:10:44 +0300

    Updates

---
+++ precache-manifest.be372e20ac6a8500318d6537064f7cb7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d59bcb0853760dcb193e6e83a40e42e",
+    "url": "/jog_app/index.html"
+  },
+  {
+    "revision": "520a144687e4c67badb0",
+    "url": "/jog_app/static/css/main.db48d1aa.chunk.css"
+  },
+  {
+    "revision": "8fffeb5d274342e9c661",
+    "url": "/jog_app/static/js/2.8b8ea398.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jog_app/static/js/2.8b8ea398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520a144687e4c67badb0",
+    "url": "/jog_app/static/js/main.3d6f4f0d.chunk.js"
+  },
+  {
+    "revision": "611d73d289341358f594",
+    "url": "/jog_app/static/js/runtime-main.50766257.js"
+  },
+  {
+    "revision": "c9a3322a389b6a97e4132a355c813dc7",
+    "url": "/jog_app/static/media/bear.c9a3322a.svg"
+  },
+  {
+    "revision": "145db14e063938646265802794cab611",
+    "url": "/jog_app/static/media/bearPink.145db14e.png"
+  },
+  {
+    "revision": "1e87aaad6cb67b02229a810920e6af16",
+    "url": "/jog_app/static/media/filter.1e87aaad.svg"
+  },
+  {
+    "revision": "f0b7b098d2d04b5e341087c0c34a37dd",
+    "url": "/jog_app/static/media/logo.f0b7b098.svg"
+  },
+  {
+    "revision": "2db1cfb80e90d0463bf460ac6c9ee6ff",
+    "url": "/jog_app/static/media/logoGreen.2db1cfb8.png"
+  },
+  {
+    "revision": "f21f2755f724e5109f487763cff05d3f",
+    "url": "/jog_app/static/media/sad.f21f2755.svg"
+  },
+  {
+    "revision": "0cfc29e3e69f8d0a6083fed65510eba4",
+    "url": "/jog_app/static/media/sfui.0cfc29e3.ttf"
+  }
+]);
